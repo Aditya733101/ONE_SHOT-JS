b1b12adff76c9f05e20617798178534ef4faa844
ADITYA SARKAR <104191788+Aditya733101@users.noreply.github.com>
2024-01-29 16:17:31 +0000

    Number

---
+++ 01besic/Number_Math.js
@@ -0,0 +1,16 @@
+                // Number syntex
+const firstNumber = 400;
+//console.log(firstNumber);
+
+const socondNumber = new Number(4000);// convert to Number 
+//console.log(socondNumber.toFixed(2));//number method 
+//console.log(socondNumber.toString().length);//number convert to string 
+
+// --------------------------------Math ------------------------------------------
+// console.log(Math)
+console.log(Math.abs(-4));//convert to negetive to positive number
+console.log(Math.round(4.5));//round to nearest number 
+console.log(Math.floor(4.2));//return the value of x rounded down to its nearest integer
+console.log(Math.random());//alaway return number value 
+const num = (Math.random()*10)+1;
+console.log(Math.floor(num));
