c7debcf9cac6a6d5172c334aee621fd176f19517
ADITYA SARKAR <104191788+Aditya733101@users.noreply.github.com>
2024-02-06 11:09:04 +0000

    map

--- 01besic/map.js
+++ 01besic/map.js
@@ -1,5 +1,28 @@
+// map() : create a new array by applying a function   each element 
 const arrNum = [1,2,3,4,5,6,7]
-const myArr = arrNum.map((item)=>{
-  return item + 10;
-})
+const myArr = arrNum
+                    .map((item)=>item+10)
+                    .map((item)=>item+1)
+                    .filter((item)=>item>15)
 console.log(myArr);
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
