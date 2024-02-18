95c7cd42cfaf3066964a4d1da64be898f9943583
ADITYA SARKAR <104191788+Aditya733101@users.noreply.github.com>
2024-02-18 10:49:20 +0000

    js

---
+++ project/project-1/main.js
@@ -0,0 +1,19 @@
+const nums = document.querySelectorAll('.button');
+const main = document.querySelector("body");
+nums.forEach(function (button){
+  button.addEventListener("click",function (e) {
+    if (e.target.id === 'grey') {
+      main.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'yellow') {
+      main.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'pink') {
+      main.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'green') {
+      main.style.backgroundColor = e.target.id;
+    }
+    
+  })
+})
